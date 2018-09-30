d4aa29293fc1e6e0ea553f686a4a0a09ee7b4ee7
Brandon L <give.me.mail.not.spam@gmail.com>
2018-09-30 16:23:25 -0400

    Updated title

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
+            <h1 className="App-title">Fibonacci Complicated Calculator</h1>
             <Link to="/">Home</Link>
             <Link to="/other-page">Other Page</Link>
           </header>
